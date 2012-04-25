ec1dc5a803155279503c0856a49d0fd39816a0ee
Alex Conner <codatory@fastmail.fm>
2012-04-25 17:27:48 -0400

    Fix controller bug

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,14 +1,14 @@
 class RootController < ApplicationController
   def index
     if params[:recipient] && params[:subject] && params[:message]
-      recipient = Recipient.find(params[:recipient])
+      recipient = Recipient.find_or_create_by_email(params[:recipient])
       message   = recipient.messages.create!({
         :subject => params[:subject],
         :message => params[:message]
       })
 
       # Deliver the message immediately in the background thread
-      QMAIL.deliver_message!(message.id) if params[:instant]
+      QMAIL.actor.deliver_message!(message.id) if params[:instant]
 
       render :json => message
     else
