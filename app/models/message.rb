a3f81bd9cd6da571d382e7de0e51b55e0b679a40
Alex Conner <codatory@fastmail.fm>
2012-04-25 17:28:23 -0400

    Add scopes to message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ActiveRecord::Base
   belongs_to :recipient
-  attr_accessible :message, :subject
+  attr_accessible :recipient, :message, :subject
+
+  scope :unsent, where(:sent_at => nil)
+  scope :aged,   lambda { where('created_at < ?', 15.minutes.ago) }
 end
