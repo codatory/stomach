525031a20590adcf64156bf32b389d0781d66c79
Alex Conner <codatory@fastmail.fm>
2012-04-25 15:17:15 -0400

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stomach::Application.config.secret_token = '22d1ce1948cb8cc75c551f87f5b457c5b121df5c7e4bdd37ceb2ed110ab0a646eb6b2bad1b673ff32296a6ccc0c12a8e08f89cae630995d86e4dfbe34b750b38'
